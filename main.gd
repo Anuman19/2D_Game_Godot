f821809e23e0a5c550b98f9abd7c9aa60b289894
naveen.anpalagan@students.ffhs.ch <naveen.anpalagan@students.ffhs.ch>
2024-02-24 15:49:54 +0100

    HUD

--- main.gd
+++ main.gd
@@ -16,11 +16,17 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+
  # Replace with function body.
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+
 
 
 func _on_start_timer_timeout():
@@ -30,6 +36,8 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
+
  # Replace with function body.
 
 
